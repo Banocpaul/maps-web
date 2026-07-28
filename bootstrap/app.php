e37db6f390bf1a80f1686b50b778acf21be1f7c6
Paul Randolf <banocpaulrandolf@gmail.com>
2026-07-28 19:10:02 +0800

    Trust Render proxy for HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,13 +11,16 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-   ->withMiddleware(function (Middleware $middleware): void {
-    $middleware->alias([
-        'permission' => \App\Http\Middleware\CheckPermission::class,
-    ]);
-})
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
+        $middleware->alias([
+            'permission' => \App\Http\Middleware\CheckPermission::class,
+        ]);
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
         );
-    })->create();
+    })
+    ->create();
